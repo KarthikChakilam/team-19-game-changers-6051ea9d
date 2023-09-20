077c2df7e9d8d111d552e6dd526bf7efd8fca84a
Karthik Mahesh <karthikmahesh9@gmail.com>
2023-09-20 16:22:31 +0000

    Fixed Default name

--- LevelUpGame/levelup/GameController.cs
+++ LevelUpGame/levelup/GameController.cs
@@ -3,7 +3,7 @@ namespace levelup
     public class GameController
     {
         // TODO: Is this what you want your default character name to be?git c
-        public readonly string DEFAULT_CHARACTER_NAME = "Bob";
+        public readonly string DEFAULT_CHARACTER_NAME = "Character";
 
         public record struct GameStatus(
             // TODO: Add other status data
